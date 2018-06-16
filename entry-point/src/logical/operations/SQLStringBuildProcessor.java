3c78ce6b5b0d80e8e7eedd22df0975a23b3c8029
Aayush Makkad <aayu.makkad@gmail.com>
2018-06-17 00:46:31 +0530

    working on complete spport for IN

--- entry-point/src/logical/operations/SQLStringBuildProcessor.java
+++ entry-point/src/logical/operations/SQLStringBuildProcessor.java
@@ -1,6 +1,9 @@
 package logical.operations;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class SQLStringBuildProcessor {
 	
@@ -48,14 +51,45 @@ public class SQLStringBuildProcessor {
 		SQLStringHolder.setMainString("where "+colName+" like  "+Condition);
 	}
 	
-	public static void SelectIn(String _colName,ArrayList<Integer> _lsInt){
+	public static void SelectIn(String _colName,ArrayList<Object> _lsInt){
 		StringBuilder sb = new StringBuilder();
-		 sb.append(_lsInt.toString());
+		ArrayList<Object> _res = new ArrayList<Object>();
+		_res = ListCleaningProcess(_lsInt);
+		 sb.append(_res.toString());
 		 sb.replace(0, 1, "(");
 		 sb.replace(sb.length()-1, sb.length(), ")");
 		 System.out.println(sb.toString());
 		 SQLStringHolder.setMainString("where "+_colName+" in "+sb.toString());
 		 
+	}
+	
+	private static ArrayList<Object> ListCleaningProcess(ArrayList<Object> _process)
+	{
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(_process.toString().charAt(0));
+		HashMap<Integer,String> _memory = new HashMap<Integer,String>();
+ 		for(Object o : _process){
+			
+			 if(o instanceof String){
+				
+				int index = _process.indexOf(o);
+				String s = String.class.cast(o);
+				String put = "'"+s+"'";
+				_memory.put(_process.indexOf(o),put);
+				
+			}
+			
+		}
+ 		Iterator<Map.Entry<Integer,String>> itr = _memory.entrySet().iterator();
+		while(itr.hasNext()){
+			Map.Entry<Integer, String> ent = itr.next();
+				
+			
+			_process.remove(ent.getValue());
+			_process.add(ent.getKey(), ent.getValue());
+		}
+		return _process;
 	}
 
 }
--- entry-point/src/logical/operations/test.java
+++ entry-point/src/logical/operations/test.java
@@ -13,12 +13,14 @@ public class test {
 		//SQLStringBuildProcessor.SelectWhereBuild("not first_test", "1");
 		
 		
-		ArrayList<Integer> _try = new ArrayList<Integer>();
+		ArrayList<Object> _try = new ArrayList<Object>();
 			_try.add(198);
 			_try.add(199);
 			_try.add(19889);
+			_try.add("supp");
 	//	SQLStringBuildProcessor.SelectLike("col_2", "%a");
 		SQLStringBuildProcessor.SelectIn("first_test",_try);
+		
 		SQLStringBuildProcessor.SelectANDBuild("not first_test", "198");
 		System.out.println(SQLStringHolder.getmainString());
 		ResultSet rs = StringBuilderJDBCProcessor.mainProcess();
