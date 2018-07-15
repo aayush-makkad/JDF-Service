2021abaa712b41aab6c36b0af828f8eaaae89778
Aayush Makkad <aayu.makkad@gmail.com>
2018-07-15 09:58:41 +0530

    parameterized select all done with mantained insertion order

--- gs-rest-service/src/main/java/hello/serviceController.java
+++ gs-rest-service/src/main/java/hello/serviceController.java
@@ -1,7 +1,12 @@
 package hello;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +16,25 @@ import process.process;
 
 @RestController
 public class serviceController {
-
+	
+	
+	LinkedHashMap<String,Object> res = new LinkedHashMap<String,Object>();
+	ArrayList<Object> get = new ArrayList<Object>();
 	
 	@RequestMapping("/service")
-	public ArrayList<Object> _serice(){
+	public HashMap<String,Object> _serice(@RequestParam(value="table", defaultValue="test_table") String tablename){
 		
-		return process.SelectAll("test_table");
 		
-	}
+		get = process.SelectAll(tablename);
+		int i =0;
+		res.clear();
+		for(Object o : get){
+			i++;
+			res.put("Column "+i+"-->", o);		
+		}
+		return res;
+		}
+	
 	
 	
 }
